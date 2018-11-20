3c003d1ac1626b785254c55702407813dc7ff3ee
Jenny <jfcong@berkeley.edu>
2018-11-19 19:28:39 -0800

    Updates

---
+++ precache-manifest.4aaf049e9b26cba3bd8f22d49cadcce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6db7eb10da1bae5c476625fcbb289f9c",
+    "url": "/home/static/media/header.6db7eb10.png"
+  },
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "51371c2a7fdd322e32a0",
+    "url": "/home/static/js/main.51371c2a.chunk.js"
+  },
+  {
+    "revision": "85716740b10a27d786dd",
+    "url": "/home/static/js/1.85716740.chunk.js"
+  },
+  {
+    "revision": "51371c2a7fdd322e32a0",
+    "url": "/home/static/css/main.f9e9ea1b.chunk.css"
+  },
+  {
+    "revision": "85716740b10a27d786dd",
+    "url": "/home/static/css/1.f5a977f5.chunk.css"
+  },
+  {
+    "revision": "19f066c30450c605e7dbc1a227df9768",
+    "url": "/home/index.html"
+  }
+];
